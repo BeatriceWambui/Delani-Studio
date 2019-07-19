1fd999d2f7e4e4620c48c321097699cc078f4c8b
BeatriceWambui <wachirabeatice2020@gmail.com>
2019-07-19 17:58:20 +0300

    make hoover effect

--- js/delani.js
+++ js/delani.js
@@ -49,9 +49,72 @@ $(document).ready(function(){
             alert("Please enter correct data.")
         }
 
-    })
-})
+    });
+});
 
+$(document).ready(function(){
+    $("#work1").hover(function(){
+        $("#name1").show();
+    }).mouseleave(function(){
+        $("#name1").hide();
+    });
+});
+
+$(document).ready(function(){
+    $("#work2").hover(function(){
+        $("#name2").show();
+    }).mouseleave(function(){
+        $("#name2").hide();
+    });
+});
+
+$(document).ready(function(){
+    $("#work3").hover(function(){
+        $("#name3").show();
+    }).mouseleave(function(){
+        $("#name3").hide();
+    });
+});
+
+$(document).ready(function(){
+    $("#work4").hover(function(){
+        $("#name4").show();
+    }).mouseleave(function(){
+        $("#name4").hide();
+    });
+});
+
+$(document).ready(function(){
+    $("#work5").hover(function(){
+        $("#name5").show();
+    }).mouseleave(function(){
+        $("#name5").hide();
+    });
+});
+
+$(document).ready(function(){
+    $("#work6").hover(function(){
+        $("#name6").show();
+    }).mouseleave(function(){
+        $("#name6").hide();
+    });
+});
+
+$(document).ready(function(){
+    $("#work7").hover(function(){
+        $("#name7").show();
+    }).mouseleave(function(){
+        $("#name7").hide();
+    });
+});
+
+$(document).ready(function(){
+    $("#work8").hover(function(){
+        $("#name8").show();
+    }).mouseleave(function(){
+        $("#name8").hide();
+    });
+});
 
 
 
