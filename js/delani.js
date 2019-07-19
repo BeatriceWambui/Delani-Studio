b32d7c8220cc6650d741f563639f698f04c30b3a
BeatriceWambui <wachirabeatice2020@gmail.com>
2019-07-19 14:40:36 +0300

    add toggle in what we do

---
+++ js/delani.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+    $("#toggle1").click(function(){
+       $("#toggle1").hide(); 
+        $("#hide1").show();
+});
+});
+$(document).ready(function(){
+    $("#hide1").click(function(){
+        $("#toggle1").show();
+       $("#hide1").hide(); 
+});
+});
+
+$(document).ready(function(){
+    $("#toggle2").click(function(){
+       $("#toggle2").hide(); 
+        $("#hide2").show();
+});
+});
+
+$(document).ready(function(){
+    $("#hide2").click(function(){
+       $("#toggle2").show(); 
+        $("#hide2").hide();
+});
+});
+
+$(document).ready(function(){
+    $("#toggle3").click(function(){
+       $("#toggle3").hide(); 
+        $("#hide3").show();
+});
+});
+
+$(document).ready(function(){
+    $("#hide3").click(function(){
+       $("#toggle3").show(); 
+        $("#hide3").hide();
+});
+});
+
